a269818e9b3d503be69319ce8233609bf7f78fe8
Luna <git@l4.pm>
2024-08-06 23:18:22 -0300

    port to zig 0.13

--- build.zig
+++ build.zig
@@ -5,12 +5,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("uuid", .{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const tests = b.addTest(.{ .root_source_file = .{ .path = "src/root.zig" } });
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+    });
     const run_tests = b.addRunArtifact(tests);
     const tests_step = b.step("test", "Run tests");
 
